12584e4f865542dfdee014ab4f8bcbd4196730d8
Sumit patidar <sumit.patidar@stayntouch.com>
2025-11-06 23:34:08 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// require("dotenv").config({path: "./.env"});
+
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import connectDB from "./db/index.js";
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Other approach to connect to MongoDB and start Express server
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constant.js";
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         app.on("error", (error) => {
+//             console.error("Error occurred:", error);
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })();
