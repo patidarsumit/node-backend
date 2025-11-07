12ea2c5410fa4d983cd8be9d6c41c5616bb4f027
Sumit patidar <sumit.patidar@stayntouch.com>
2025-11-07 11:35:54 +0530

    Implement user and video models; add JWT and bcrypt for authentication

---
+++ src/models/video.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    title: { type: String, required: true, trim: true, index: true },
+    description: { type: String, required: true, trim: true },
+    duration: { type: Number, required: true },
+    views: { type: Number, default: 0 },
+    isPublished: { type: Boolean, default: true },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
