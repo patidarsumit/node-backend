04165527b3f711d31eb973556bc8c10b046b474b
Sumit patidar <sumit.patidar@stayntouch.com>
2025-11-14 01:10:43 +0530

    Implement JWT authentication: add verifyJWT middleware and update user routes for login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import User from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false }); // Save without running validators
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Error generating tokens");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // Registration logic here
   // return res.status(200).json({ message: "User registered successfully" });
@@ -20,10 +35,14 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existingUser) {
     throw new ApiError("409, User with given email or username already exists");
   }
-  
+
   const avatarLocalPath = req.files?.avatar[0]?.path;
   let coverImageLocalPath;
-  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
     coverImageLocalPath = req.files.coverImage[0].path;
   }
 
@@ -63,4 +82,74 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // Login logic here
+  const { username, password } = req.body;
+
+  if ([username, password].some((field) => field.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // const user  = await User.findOne({ $or: [{ email: username.toLowerCase() }, { username: username.toLowerCase() }] });
+  const user = await User.findOne({ username: username.toLowerCase() });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  // Generate tokens
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -__v"
+  ); //Exclude password, refreshToken, and __v fields
+
+  // Send tokens in response
+  const options = {
+    httpOnly: true,
+    secure: true,
+    // sameSite: "Strict",
+    // maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  };
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const user = await User.findByIdAndUpdate(userId, {
+    $set: { refreshToken: undefined },
+  });
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+    // sameSite: "Strict",
+    // maxAge: 0, // Expire immediately
+  };
+
+  return res
+    .status(200)
+    .clearCookie("refreshToken", options)
+    .clearCookie("accessToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  // replace res with _ to avoid unused variable warning
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError("401, Access token is missing");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -__v"
+    );
+
+    if (!user) {
+      throw new ApiError(404, "Invalid token - user not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expired access token");
+  }
+});
